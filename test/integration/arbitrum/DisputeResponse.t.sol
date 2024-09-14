b6a5ac6a6ea62cd275f18dfc575b5229388fd627
0xJabberwock <0xjabberwock@defi.sucks>
2024-09-13 23:48:42 -0300

    test: expect reversions in integration tests

--- test/integration/arbitrum/DisputeResponse.t.sol
+++ test/integration/arbitrum/DisputeResponse.t.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.26;
 import './IntegrationBase.sol';
 
 contract IntegrationDisputeResponse is IntegrationBase {
+  using ValidatorLib for IOracle.Request;
+  using ValidatorLib for IOracle.Response;
+
   function setUp() public override {
     super.setUp();
 
@@ -26,6 +29,27 @@ contract IntegrationDisputeResponse is IntegrationBase {
     bytes32 _requestId = _createRequest();
     // Propose the response
     bytes32 _responseId = _proposeResponse(_requestId);
+
+    // Pass the dispute window
+    vm.roll(block.number + disputeDisputeWindow + 1);
+
+    // Revert if the dispute window has passed
+    vm.expectRevert(IBondEscalationModule.BondEscalationModule_DisputeWindowOver.selector);
+    _disputeResponse(_requestId, _responseId);
+
+    // Do not pass the dispute window
+    vm.roll(block.number - disputeDisputeWindow - 1);
+
+    // Pass the dispute deadline
+    vm.warp(disputeDeadline + 1);
+
+    // Revert if the bond escalation deadline has passed
+    vm.expectRevert(IBondEscalationModule.BondEscalationModule_BondEscalationOver.selector);
+    _disputeResponse(_requestId, _responseId);
+
+    // Do not pass the dispute deadline
+    vm.warp(disputeDeadline);
+
     // Dispute the response
     bytes32 _disputeId = _disputeResponse(_requestId, _responseId);
 
@@ -37,5 +61,9 @@ contract IntegrationDisputeResponse is IntegrationBase {
     IBondEscalationModule.BondEscalation memory _escalation = bondEscalationModule.getEscalation(_requestId);
     assertEq(uint8(_escalation.status), uint8(IBondEscalationModule.BondEscalationStatus.Active));
     assertEq(_escalation.disputeId, _disputeId);
+
+    // Revert if the response has already been disputed
+    vm.expectRevert(abi.encodeWithSelector(IOracle.Oracle_ResponseAlreadyDisputed.selector, _responseId));
+    _disputeResponse(_requestId, _responseId);
   }
 }
--- test/integration/arbitrum/ProposeResponse.t.sol
+++ test/integration/arbitrum/ProposeResponse.t.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.26;
 import './IntegrationBase.sol';
 
 contract IntegrationProposeResponse is IntegrationBase {
+  using ValidatorLib for IOracle.Request;
+
   function setUp() public override {
     super.setUp();
 
@@ -24,10 +26,25 @@ contract IntegrationProposeResponse is IntegrationBase {
   function test_ProposeResponse() public {
     // Create the request
     bytes32 _requestId = _createRequest();
+
+    // Pass the response deadline
+    vm.warp(responseDeadline);
+
+    // Revert if the response is proposed after the deadline
+    vm.expectRevert(IBondedResponseModule.BondedResponseModule_TooLateToPropose.selector);
+    _proposeResponse(_requestId);
+
+    // Do not pass the response deadline
+    vm.warp(responseDeadline - 1);
+
     // Propose the response
     bytes32 _responseId = _proposeResponse(_requestId);
 
     assertEq(oracle.responseCreatedAt(_responseId), block.number);
     assertEq(bondEscalationAccounting.bondedAmountOf(_proposer, graphToken, _requestId), responseBondSize);
+
+    // Revert if the response has already been proposed
+    vm.expectRevert(IOracle.Oracle_InvalidResponseBody.selector);
+    _proposeResponse(_requestId);
   }
 }
